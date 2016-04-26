c6a42cf3b1d4ce3797567ffe1613477bf859c5e9
thadeu.batista <thadeu.batista@gmail.com>
2016-04-25 21:07:10 -0300

    modified way how HeatMap show data empty. this avoid the error null pointer exception. what you think about this?

--- src/main/java/com/numetriclabz/numandroidcharts/HeatMap.java
+++ src/main/java/com/numetriclabz/numandroidcharts/HeatMap.java
@@ -13,6 +13,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -42,8 +43,19 @@ public class HeatMap extends View {
         textPaint.setDither(true);
         textPaint.setTextSize(20);
 
-        block_height = width /columns.size();
-        block_width = (width - getRowTextWidth())/ rows.size() - 5;
+        if(columns == null){
+            columns = new ArrayList<>();
+            block_height = width / 1;
+        }else{
+            block_height = width /columns.size();
+        }
+        if(rows == null){
+            rows = new ArrayList<>();
+            block_width = (width - getRowTextWidth())/ 1;
+        }else{
+            block_width = (width - getRowTextWidth())/ rows.size() - 5;
+        }
+
     }
 
     @Override
@@ -66,44 +78,48 @@ public class HeatMap extends View {
         int valIndex = 0;
         float rowTop = 20;
         float rowBottom = block_height  + 20;
+        if(rows != null && !rows.isEmpty()) {
+            for (int rowIndex = 0; rowIndex < rows.size(); rowIndex++) {
 
-        for(int rowIndex = 0; rowIndex < rows.size(); rowIndex++){
-
-            float colLeft = 20 + getRowTextWidth();
-            float colRight = block_width + 20 + getRowTextWidth();
-
-
-            for (int colIndex = 0; colIndex < columns.size(); colIndex++){
-
-                String color = fillColor(data.get(valIndex).getHeat_value());
-                RectF rect = new RectF(colLeft, rowTop, colRight, rowBottom);
+                float colLeft = 20 + getRowTextWidth();
+                float colRight = block_width + 20 + getRowTextWidth();
 
 
-                paint.setColor(Color.parseColor(color));
-                paint.setStyle(Paint.Style.FILL);
-                canvas.drawRect(rect, paint);
-                paint.setColor(Color.BLACK);
-                paint.setStyle(Paint.Style.STROKE);
-                canvas.drawRect(rect, paint);
+                for (int colIndex = 0; colIndex < columns.size(); colIndex++) {
 
-                String value = data.get(valIndex).getHeat_value()+"";
-                canvas.drawText(value, (colRight + colLeft)/2, (rowTop + rowBottom)/2, textPaint);
+                    String color = fillColor(data.get(valIndex).getHeat_value());
+                    RectF rect = new RectF(colLeft, rowTop, colRight, rowBottom);
 
-                colLeft += block_width;
-                colRight += block_width;
-                valIndex++;
+
+                    paint.setColor(Color.parseColor(color));
+                    paint.setStyle(Paint.Style.FILL);
+                    canvas.drawRect(rect, paint);
+                    paint.setColor(Color.BLACK);
+                    paint.setStyle(Paint.Style.STROKE);
+                    canvas.drawRect(rect, paint);
+
+                    String value = data.get(valIndex).getHeat_value() + "";
+                    canvas.drawText(value, (colRight + colLeft) / 2, (rowTop + rowBottom) / 2, textPaint);
+
+                    colLeft += block_width;
+                    colRight += block_width;
+                    valIndex++;
+                }
+
+                textPaint.setTextSize(24);
+                String value = rows.get(rowIndex).getLabels();
+                canvas.drawText(value, 10, (rowTop + rowBottom) / 2, textPaint);
+
+                rowTop += block_height;
+                rowBottom += block_height;
             }
-
-            textPaint.setTextSize(24);
-            String value = rows.get(rowIndex).getLabels();
-            canvas.drawText(value, 10, (rowTop + rowBottom)/2, textPaint);
-
-            rowTop += block_height;
-            rowBottom +=block_height;
+            plotXAxisLabels(canvas, (rowBottom - block_height + 20));
+            addLegends(canvas, (int)rowBottom, (int)(20 + getRowTextWidth()));
+        }else{
+            canvas.drawText("No data information", 10, 50, textPaint);
         }
 
-        plotXAxisLabels(canvas, (rowBottom - block_height + 20));
-        addLegends(canvas, (int)rowBottom, (int)(20 + getRowTextWidth()));
+
     }
 
 
@@ -111,14 +127,15 @@ public class HeatMap extends View {
 
         float colLeft = 20 ;
         float colRight = block_width ;
+        if(columns != null && !columns.isEmpty()) {
+            for (int colIndex = 0; colIndex < columns.size(); colIndex++) {
 
-        for (int colIndex = 0; colIndex < columns.size(); colIndex++){
+                String value = columns.get(colIndex).getLabels();
 
-            String value = columns.get(colIndex).getLabels();
-
-            canvas.drawText(value, (colLeft + colRight)/2 + 30, bottom + getColTextHeight(), textPaint);
-            colLeft += block_width;
-            colRight += block_width;
+                canvas.drawText(value, (colLeft + colRight) / 2 + 30, bottom + getColTextHeight(), textPaint);
+                colLeft += block_width;
+                colRight += block_width;
+            }
         }
     }
 
@@ -150,13 +167,14 @@ public class HeatMap extends View {
         textPaint.setTextSize(24);
 
         float text_width = 0;
+        if(rows != null && !rows.isEmpty()) {
+            for (int rowIndex = 0; rowIndex < rows.size(); rowIndex++) {
 
-        for(int rowIndex = 0; rowIndex < rows.size(); rowIndex++){
+                textPaint.getTextBounds("Three", 0, "Three".length(), bounds);
 
-            textPaint.getTextBounds("Three", 0, "Three".length(), bounds);
-
-            if(bounds.width() > text_width)
-                text_width = bounds.width();
+                if (bounds.width() > text_width)
+                    text_width = bounds.width();
+            }
         }
         return text_width;
     }
@@ -168,14 +186,15 @@ public class HeatMap extends View {
         textPaint.setTextSize(24);
 
         float text_width = 0;
+        if(rows != null && !rows.isEmpty()) {
+            for (int rowIndex = 0; rowIndex < rows.size(); rowIndex++) {
 
-        for(int rowIndex = 0; rowIndex < rows.size(); rowIndex++){
+                //String row = rows.get(rowIndex).getRows();
+                textPaint.getTextBounds("Three", 0, "Three".length(), bounds);
 
-            //String row = rows.get(rowIndex).getRows();
-            textPaint.getTextBounds("Three", 0, "Three".length(), bounds);
-
-            if(bounds.width() > text_width)
-                text_width = bounds.height();
+                if (bounds.width() > text_width)
+                    text_width = bounds.height();
+            }
         }
         return text_width;
     }
@@ -184,22 +203,22 @@ public class HeatMap extends View {
     private String fillColor(int value){
 
         String color = "";
+        if(limits != null && limits.length() > 0) {
+            for (int index = 0; index < limits.length(); index++) {
 
-        for(int index = 0; index < limits.length(); index++){
+                try {
+                    JSONObject limit = (JSONObject) limits.get(index);
 
-            try {
-                JSONObject limit = (JSONObject)limits.get(index);
+                    int min = limit.getInt("minvalue");
+                    int max = limit.getInt("maxvalue");
 
-                int min = limit.getInt("minvalue");
-                int max = limit.getInt("maxvalue");
-
-                if(value >= min && value < max){
-                    color =  limit.getString("colorcode");
-                    break;
+                    if (value >= min && value < max) {
+                        color = limit.getString("colorcode");
+                        break;
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
                 }
-            }
-            catch (JSONException e){
-                e.printStackTrace();
             }
         }
         return color;
@@ -208,25 +227,25 @@ public class HeatMap extends View {
     private void addLegends(Canvas canvas, int top, int left){
 
         legends = new RectF(left, top, width, height);
+        if(limits != null && limits.length() > 0) {
+            for (int index = 0; index < limits.length(); index++) {
 
-        for(int index = 0; index < limits.length(); index++){
+                try {
+                    JSONObject limit = (JSONObject) limits.get(index);
 
-            try {
-                JSONObject limit = (JSONObject)limits.get(index);
+                    String label = limit.getString("label");
+                    String color = limit.getString("colorcode");
 
-                String label = limit.getString("label");
-                String color =  limit.getString("colorcode");
-
-                Rect r = new Rect(left, top, left + 30, top + 30);
-                paint.setColor(Color.parseColor(color));
-                paint.setStyle(Paint.Style.FILL_AND_STROKE);
-                canvas.drawRect(r, paint);
-                canvas.drawText(label, left + 40, top + 20, textPaint);
-                float text_width = textPaint.measureText(label, 0, label.length());
-                left += (text_width + 60) ;
-            }
-            catch (JSONException e){
-                e.printStackTrace();
+                    Rect r = new Rect(left, top, left + 30, top + 30);
+                    paint.setColor(Color.parseColor(color));
+                    paint.setStyle(Paint.Style.FILL_AND_STROKE);
+                    canvas.drawRect(r, paint);
+                    canvas.drawText(label, left + 40, top + 20, textPaint);
+                    float text_width = textPaint.measureText(label, 0, label.length());
+                    left += (text_width + 60);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
